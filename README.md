66857b84aa9b2516f84029f74d660bcfbf76d8d7
Nenad Mikša <nenad.miksa@microblink.com>
2020-01-03 20:20:50 +0100

    add README and improve bootstrap

---
+++ README.md
@@ -0,0 +1,18 @@
+Zagreb C++ Meetup 2020-01-16
+========
+
+This repository contains code samples and presentation slides shown during C++ meetup on January 16th, 2020.
+
+[Slides](https://zagreb-cpp-user-group.github.io/meetup-2020-01-16/) are created with markdown and compiled to HTML using [Marpit](https://marpit.marp.app).
+
+# Building and running the example
+
+You will need [CMake](https://cmake.org), [Ninja](https://ninja-build.org) and [Conan](https://conan.io) installed on your computer and available in your `PATH`. If running on windows, please use a bash shell.
+
+1. Run the `bootstrap-emscripten-conan.sh` script - it will setup your conan environment for development with emscripten, download the Emscripten conan package and generate an `activate.sh` shell script that will setup your shell for emscripten development (similar to how python virtualenv works).
+1. Activate the Emscripten environment with `source ./activate.sh`
+1. Create and enter an empty build directory with `mkdir build && cd build`
+1. Initialize the cmake build directory with `ecmake -DCMAKE_BUILD_TYPE=Debug ..`
+1. Build the code with `ninja` (or with `cmake --build .`)
+1. Serve the `bin` directory with simple HTTP server. For example with `python3 -m http.server --directory ./bin 8080`
+1. Navigate your browser to `http://localhost:8080` and allow camera access.
--- bootstrap-emscripten-conan.sh
+++ bootstrap-emscripten-conan.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+hash cmake 2>/dev/null || { echo >&2 "CMake is required but it's not installed. Aborting."; exit 1; }
+hash conan 2>/dev/null || { echo >&2 "Conan is required but it's not installed. Aborting."; exit 1; }
+hash ninja 2>/dev/null || { echo >&2 "Ninja is required but it's not installed. Aborting."; exit 1; }
+
 # First, setup conan with required remotes
 conan remote add dodoent https://api.bintray.com/conan/dodoent/conan || true
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan || true
 
-# Finally, install conan profiles
+# Install conan profiles
 conan config install conan-config.zip
+
+# Install the emsdk installer
+conan install emsdk_installer/1.38.29@bincrafters/stable
+
+# detect conan home location
+CONAN_PREFIX=$CONAN_USER_HOME
+
+if [ -z "$CONAN_PREFIX" ]; then
+    CONAN_PREFIX=$HOME
+fi
+
+# generate activate.sh
+EMSDK=$CONAN_PREFIX/.conan/data/emsdk_installer/1.38.29/bincrafters/stable/package/16edc5c715408e005b54d61db41959d8f63a70c2
+EMSCRIPTEN=$EMSDK/emscripten/1.38.29
+EM_CONFIG=$EMSDK/.emscripten
+EM_CACHE=$EMSDK/.emscripten_cache
+CMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake
+
+echo "export EMSDK=$EMSDK
+export EMSCRIPTEN=$EMSCRIPTEN
+export EM_CONFIG=$EM_CONFIG
+export EM_CACHE=$EM_CACHE
+export PATH=\$PATH:$EMSCRIPTEN
+
+alias ecmake=\"cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE\"" > ./activate.sh
+
+echo "Use 'source ./activate.sh' to activate the Emscripten environment for current shell session and use 'ecmake' instead of 'cmake' for build system generation."
